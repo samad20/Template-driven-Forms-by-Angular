46597ffc3af13923078471d402aa194251af28d4
samad20 <musab.aljahdhami@gmail.com>
2023-02-23 13:17:14 +0400

    add contactMethods array for droplistdown

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.scss']
+  styleUrls: ['./contact-form.component.scss'],
 })
 export class ContactFormComponent {
-
+  contactMethods = [
+    { id: 1, name: 'Email' },
+    { id: 2, name: 'Phone' },
+    { id: 3, name: 'WhatsApp' },
+  ];
 }
